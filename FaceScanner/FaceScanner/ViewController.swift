c8d37da73766b0ca6f324478579817237f5f3d3d
Kevin Chiang <kevin.chiang@silkrode.com.tw>
2021-10-07 14:23:44 +0800

    init project

---
+++ FaceScanner/FaceScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceScanner
+//
+//  Created by 江翰臻 on 2021/10/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
