35fd95a52d3aed00aec819226b5c0ffef3d0244a
Kevin Chiang <kevin.chiang@silkrode.com.tw>
2021-10-21 16:31:06 +0800

    feat(Home): 選擇銀行 Layout

--- FaceScanner/FaceScanner/View Controller/Home/HomeViewController.swift
+++ FaceScanner/FaceScanner/View Controller/Home/HomeViewController.swift
@@ -9,22 +9,47 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var startButtonView: UIView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        initLayout()
+    struct Constants {
+        static let banks = ["兴业银行", "邮政银行", "中国银行", "中国工商银行", "中国农业银行"]
     }
+
+    // MARK: - View Components
+
+    @IBOutlet weak var bankTextField: UITextField! {
+        didSet {
+            bankTextField.text = nil
+            bankTextField.textColor = UIColor(named: "myPurple")
+            bankTextField.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+
+            bankTextField.delegate = self
+            bankTextField.placeholder = "请选择银行"
+        }
+    }
+
+    @IBOutlet weak var bankSelectionView: UIView! {
+        didSet {
+            let tapGR = UITapGestureRecognizer(target: self, action: #selector(bankSelectionViewTapped))
+            bankSelectionView.addGestureRecognizer(tapGR)
+            bankSelectionView.isUserInteractionEnabled = true
+        }
+    }
+
+    @IBOutlet weak var startButtonView: UIView! {
+        didSet {
+            startButtonView.layer.cornerRadius = 10
+            startButtonView.clipsToBounds = true
+            let tapGR = UITapGestureRecognizer(target: self, action: #selector(startButtonViewTapped))
+            startButtonView.addGestureRecognizer(tapGR)
+            startButtonView.isUserInteractionEnabled = true
+        }
+    }
+
 }
 
 private extension HomeViewController {
 
-    func initLayout() {
-        let tapGR = UITapGestureRecognizer(target: self, action: #selector(startButtonViewTapped))
-        startButtonView.addGestureRecognizer(tapGR)
-        startButtonView.isUserInteractionEnabled = true
-        startButtonView.layer.cornerRadius = 10
+    @objc func bankSelectionViewTapped() {
+        bankTextField.becomeFirstResponder()
     }
 
     @objc func startButtonViewTapped() {
@@ -33,5 +58,27 @@ private extension HomeViewController {
         scannerVC.modalPresentationStyle = .fullScreen
         scannerVC.modalTransitionStyle = .crossDissolve
         self.present(scannerVC, animated: true, completion: nil)
+    }
+
+    func showBankSelectionSheet() {
+        let alert = UIAlertController()
+        for bank in Constants.banks {
+            let action = UIAlertAction(title: bank, style: .default) { [weak self] _ in
+                self?.bankTextField.text = bank
+            }
+            alert.addAction(action)
+        }
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension HomeViewController: UITextFieldDelegate {
+    // MARK: - UITextFieldDelegate
+
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        showBankSelectionSheet()
+        return false
     }
 }
